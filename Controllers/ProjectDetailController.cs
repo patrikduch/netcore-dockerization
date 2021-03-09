2650bf3aafa67d31c6fae01af9d1d35108f92e21
patrikduch <duchpatrik@icloud.com>
2021-03-09 22:09:27 +0100

    NetCore - ProjectDetail endpoint

---
+++ Controllers/ProjectDetailController.cs
@@ -0,0 +1,23 @@
+﻿// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace NetCore_Dockerization.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using NetCore_Dockerization.Models;
+
+    [Route("api/project")]
+    [ApiController]
+    public class ProjectDetailController : ControllerBase
+    {
+        // GET: api/project
+        [HttpGet]
+        public ProjectDetail GetProjectDetail()
+        {
+            return new ProjectDetail
+            {
+                Id = 1,
+                Name = "Test application in .NETCore on AWS cloud platform",
+                Author = "Patrik Duch"
+            };
+        }
+    }
+}
---
+++ Models/ProjectDetail.cs
@@ -0,0 +1,24 @@
+﻿namespace NetCore_Dockerization.Models
+{
+    /// <summary>
+    /// ProjectDetail model class, that represents all basic information about web project.
+    /// </summary>
+    public class ProjectDetail
+    {
+        /// <summary>
+        ///  Gets or sets project identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets name of web project.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets author name of this app.
+        /// </summary>
+        public string Author { get; set; }
+
+    }
+}
